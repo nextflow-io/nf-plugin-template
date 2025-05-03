0ccdf363b908c4e004ababc6d846c8599927c84b
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2025-05-03 17:12:18 +0200

    Add basic test
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

---
+++ src/test/groovy/nextflow/plugin/template/MyObserverTest.groovy
@@ -0,0 +1,22 @@
+package nextflow.plugin.template
+
+import nextflow.Session
+import spock.lang.Specification
+
+/**
+ * Implements a basic factory test
+ *
+ */
+class MyObserverTest extends Specification {
+
+    def 'should create the observer instance' () {
+        given:
+        def factory = new MyFactory()
+        when:
+        def result = factory.create(Mock(Session))
+        then:
+        result.size() == 1
+        result.first() instanceof MyObserver
+    }
+
+}
